dd369990ca1cac84ca64c66e0191b0b7785eb933
hiroakissh <hiroaki.saito1014@gmail.com>
2024-04-17 23:16:43 +0900

    [add] add project

---
+++ tca-sample/tca_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_sampleApp.swift
+//  tca-sample
+//
+//  Created by HiroakiSaito on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct tca_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
